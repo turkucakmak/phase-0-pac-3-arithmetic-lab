97161867a54de4252e5c4025a03d625c7d9a7973
turku <turkucakmak@hotmail.com>
2023-05-04 17:08:02 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a,b){
+return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function increment(n){
+    return n+1;
+}
+
+function decrement(n){
+   return n-1;
+}
+
+function makeInt(n){
+return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+return parseFloat(n);
+}
+
+add(3,4);
+subtract(3,4);
+multiply(3,4);
+divide(3,4);
+increment(5);
+decrement(5);
+makeInt(4);
+preserveDecimal(5);
